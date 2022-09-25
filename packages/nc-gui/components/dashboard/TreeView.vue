c15c7d016dc2ce532a7e2eec28b55ba85605b54c
mertmit <mertmit99@gmail.com>
2022-09-25 16:43:16 +0300

    feat: team & settings modal data sources tab revised
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/dashboard/TreeView.vue
+++ packages/nc-gui/components/dashboard/TreeView.vue
@@ -138,7 +138,7 @@ const addTableTab = (table: TableType) => {
   addTab({ title: table.title, id: table.id, type: table.type as TabType })
 }
 
-function openRenameTableDialog(table: TableType, rightClick = false) {
+function openRenameTableDialog(table: TableType, baseId: string, rightClick = false) {
   $e(rightClick ? 'c:table:rename:navdraw:right-click' : 'c:table:rename:navdraw:options')
 
   const isOpen = ref(true)
@@ -146,6 +146,7 @@ function openRenameTableDialog(table: TableType, rightClick = false) {
   const { close } = useDialog(resolveComponent('DlgTableRename'), {
     'modelValue': isOpen,
     'tableMeta': table,
+    'baseId': baseId,
     'onUpdate:modelValue': closeDialog,
   })
 
@@ -363,7 +364,7 @@ const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
 
                           <template #overlay>
                             <a-menu class="!py-0 rounded text-sm">
-                              <a-menu-item v-if="isUIAllowed('table-rename')" @click="openRenameTableDialog(table)">
+                              <a-menu-item v-if="isUIAllowed('table-rename')" @click="openRenameTableDialog(table, base.id)">
                                 <div class="nc-project-menu-item">
                                   {{ $t('general.rename') }}
                                 </div>
@@ -520,7 +521,7 @@ const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
 
                             <template #overlay>
                               <a-menu class="!py-0 rounded text-sm">
-                                <a-menu-item v-if="isUIAllowed('table-rename')" @click="openRenameTableDialog(table)">
+                                <a-menu-item v-if="isUIAllowed('table-rename')" @click="openRenameTableDialog(table, base.id)">
                                   <div class="nc-project-menu-item">
                                     {{ $t('general.rename') }}
                                   </div>
@@ -552,7 +553,10 @@ const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
       <template v-if="!isSharedBase" #overlay>
         <a-menu class="!py-0 rounded text-sm">
           <template v-if="contextMenuTarget.type === 'table'">
-            <a-menu-item v-if="isUIAllowed('table-rename')" @click="openRenameTableDialog(contextMenuTarget.value, true)">
+            <a-menu-item
+              v-if="isUIAllowed('table-rename')"
+              @click="openRenameTableDialog(contextMenuTarget.value, base.id, true)"
+            >
               <div class="nc-project-menu-item">
                 {{ $t('general.rename') }}
               </div>
@@ -579,6 +583,10 @@ const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
     <a-divider class="!my-0" />
 
     <div class="flex items-start flex-col justify-start px-2 py-3 gap-2">
+      <LazyGeneralAddBaseButton
+        class="color-transition py-1.5 px-2 text-primary font-bold cursor-pointer select-none hover:text-accent"
+      />
+
       <LazyGeneralShareBaseButton
         class="color-transition py-1.5 px-2 text-primary font-bold cursor-pointer select-none hover:text-accent"
       />
