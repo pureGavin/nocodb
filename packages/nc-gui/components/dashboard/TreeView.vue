7a07dd7ae728738372ee8c2d4d3a979e5abb7d29
mertmit <mertmit99@gmail.com>
2022-10-05 03:51:42 +0300

    fix: ui improvements for multiple source interfaces
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/dashboard/TreeView.vue
+++ packages/nc-gui/components/dashboard/TreeView.vue
@@ -37,6 +37,8 @@ const [searchActive, toggleSearchActive] = useToggle()
 
 let key = $ref(0)
 
+const activeKey = ref([])
+
 const menuRef = $ref<HTMLLIElement>()
 
 const filterQuery = $ref('')
@@ -386,7 +388,7 @@ function openTableCreateDialog(baseId?: string) {
                     </GeneralTooltip>
                   </div>
                 </div>
-                <a-collapse v-else expand-icon-position="right" :bordered="false" ghost>
+                <a-collapse v-else v-model:activeKey="activeKey" expand-icon-position="right" :bordered="false" accordion ghost>
                   <a-collapse-panel :key="index">
                     <template #header>
                       <div v-if="index !== '0'" class="flex items-center gap-2">
@@ -712,6 +714,6 @@ function openTableCreateDialog(baseId?: string) {
 }
 
 :deep(.ant-collapse-header) {
-  @apply !border-0 !border-y-1;
+  @apply !border-0;
 }
 </style>
